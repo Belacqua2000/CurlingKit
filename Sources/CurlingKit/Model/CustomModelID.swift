422d7681b796fccb4befb9af7c1da96db0a5fada
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-10-25 14:06:41 +0100

    Conformed CustomModelID to NSItemProviderReading and NSItemProviderWriting.

--- Sources/CurlingKit/Model/CustomModelID.swift
+++ Sources/CurlingKit/Model/CustomModelID.swift
@@ -6,10 +6,33 @@
 //
 
 import CoreTransferable
+import UniformTypeIdentifiers
 
-public struct CustomModelID: Transferable, Codable {
+public class CustomModelID: NSObject, Transferable, Codable, NSItemProviderReading, NSItemProviderWriting {
+    public static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
+        try JSONDecoder().decode(Self.self, from: data)
+    }
+    
+    public func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, (any Error)?) -> Void) -> Progress? {
+        do {
+            let data = try JSONEncoder().encode(self)
+            completionHandler(data, nil)
+        } catch {
+            completionHandler(nil, error)
+        }
+        return nil
+    }
+    
+    public static let readableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
+    public static let writableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
+    
+    
     public let id: UUID
     public static var transferRepresentation: some TransferRepresentation {
         CodableRepresentation(contentType: .modelID)
+    }
+    
+    public required init(id: UUID) {
+        self.id = id
     }
 }
