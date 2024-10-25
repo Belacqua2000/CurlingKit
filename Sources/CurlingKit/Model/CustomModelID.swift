bb825874b52d8b12cdebff53d26f0e6023b5d83d
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2024-10-25 14:28:44 +0100

    Removed NSItemProvider support.

--- Sources/CurlingKit/Model/CustomModelID.swift
+++ Sources/CurlingKit/Model/CustomModelID.swift
@@ -8,23 +8,23 @@
 import CoreTransferable
 import UniformTypeIdentifiers
 
-public class CustomModelID: NSObject, Transferable, Codable, NSItemProviderReading, NSItemProviderWriting {
-    public static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
-        try JSONDecoder().decode(Self.self, from: data)
-    }
-    
-    public func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, (any Error)?) -> Void) -> Progress? {
-        do {
-            let data = try JSONEncoder().encode(self)
-            completionHandler(data, nil)
-        } catch {
-            completionHandler(nil, error)
-        }
-        return nil
-    }
-    
-    public static let readableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
-    public static let writableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
+public struct CustomModelID: /*NSObject, */Transferable, Codable/*, NSItemProviderReading, NSItemProviderWriting */{
+//    public static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
+//        try JSONDecoder().decode(Self.self, from: data)
+//    }
+//    
+//    public func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, (any Error)?) -> Void) -> Progress? {
+//        do {
+//            let data = try JSONEncoder().encode(self)
+//            completionHandler(data, nil)
+//        } catch {
+//            completionHandler(nil, error)
+//        }
+//        return nil
+//    }
+//    
+//    public static let readableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
+//    public static let writableTypeIdentifiersForItemProvider: [String] = [UTType.modelID.identifier]
     
     
     public let id: UUID
@@ -32,7 +32,7 @@ public class CustomModelID: NSObject, Transferable, Codable, NSItemProviderReadi
         CodableRepresentation(contentType: .modelID)
     }
     
-    public required init(id: UUID) {
-        self.id = id
-    }
+//    public required init(id: UUID) {
+//        self.id = id
+//    }
 }
